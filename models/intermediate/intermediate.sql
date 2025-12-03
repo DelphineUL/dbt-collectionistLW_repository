fed0b5b8be7e48e3d9d24bd740255d66e114d516
Juliette Gompertz <Juliette@Gompertz.fr>
2025-12-03 10:00:34 +0000

    commit

--- models/intermediate/intermediate.sql
+++ models/intermediate/intermediate.sql
@@ -1,1 +1,4 @@
-SELECT * FROM {{ ref('stg_lewagonxlecollectionist__Lead_Scoring') }}
+Select
+SAFE_CAST(commission AS FLOAT64) AS commission_clean
+From {{ ref('stg_lewagonxlecollectionist__Lead_Scoring') }}
+WHERE commission_clean IS NOT NULL
