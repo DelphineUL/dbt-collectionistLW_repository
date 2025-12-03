a627d6a475e761c8bc6ec659a8b90a22a626313d
Delphine Lam <lamdelphine123@gmail.com>
2025-12-03 10:59:37 +0000

    commit data

--- models/intermediate/test.sql
+++
@@ -1,5 +0,0 @@
-select
---CAST(budget_per_night  AS FLOAT64) AS budget_per_night,
-CAST(budget  AS FLOAT64) AS budget
---CAST(client_rental_amount AS FLOAT64) as client_rental
-from {{ ref('stg_lewagonxlecollectionist__Lead_Scoring') }}
